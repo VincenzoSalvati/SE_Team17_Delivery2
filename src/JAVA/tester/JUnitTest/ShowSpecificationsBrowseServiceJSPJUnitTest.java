7e40e0e25f73cfd567423da454091e9a977b9ace
ThomasMalory <43402055+ThomasMalory@users.noreply.github.com>
2020-12-15 20:26:33 +0100

    Update ShowSpecificationsBrowseServiceJSPJUnitTest.java

--- src/JAVA/tester/JUnitTest/ShowSpecificationsBrowseServiceJSPJUnitTest.java
+++ src/JAVA/tester/JUnitTest/ShowSpecificationsBrowseServiceJSPJUnitTest.java
@@ -25,12 +25,12 @@ public class ShowSpecificationsBrowseServiceJSPJUnitTest extends TestCase {
         service = new ShowSpecificationsBrowseServiceJSP();
     }
 
-    @Order(2)
+    @Order(3)
     protected void tearDown() {
         service = null;
     }
 
-    @Order(3)
+    @Order(2)
     public final void testGetShowSpecificationsBrowseToJSONJSP() {
         String jsonResultExpected = "[{\"id\":\"0\",\"work_note\":\"The plant is closed from 00/00/20 to 00/00/20; On the remaining days, it is possible to intervene only after 10:00\",\"int_des\":\"Replacement of robot 20 welding cables\",\"id_activity\":\"0 - Fisciano - Molding\",\"week_activity\":\"1\",\"skill\":\"- PAV Certification<br>- Electrical Maintainance<br>- Knowledge of cables types<br>- XYZ-type robot knowledge<br>\"}]";
         String jsonResultActual = service.getShowSpecificationsBrowseToJSONJSP(db, 0, 1);
