050677cc568f88e3a63cf5438c01e3df3c132e78
ThomasMalory <43402055+ThomasMalory@users.noreply.github.com>
2020-12-15 13:29:03 +0100

    Update ShowSpecificationsBrowseServiceJSPTester.java

--- src/JAVA/tester/ShowSpecificationsBrowseServiceJSPTester.java
+++ src/JAVA/tester/ShowSpecificationsBrowseServiceJSPTester.java
@@ -7,9 +7,7 @@ public class ShowSpecificationsBrowseServiceJSPTester {
 
     public static void main(String[] args) {
         //Database initialization
-        MySqlDbConnection db = new MySqlDbConnection();
-        _0_SetDatabaseTest set = new _0_SetDatabaseTest();
-        set.setDatabase(db);
+        MySqlDbConnection db = MySqlDbConnection.getInstance();
         //Test JSON
         ShowSpecificationsBrowseServiceJSP service = new ShowSpecificationsBrowseServiceJSP();
         System.out.println(service.getShowSpecificationsBrowseToJSONJSP(db, 2, 1));
