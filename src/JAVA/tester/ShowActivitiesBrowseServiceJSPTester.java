80fab20576c2f101eb91068f6476c766b4de2500
ThomasMalory <43402055+ThomasMalory@users.noreply.github.com>
2020-12-15 13:28:29 +0100

    Update ShowActivitiesBrowseServiceJSPTester.java

--- src/JAVA/tester/ShowActivitiesBrowseServiceJSPTester.java
+++ src/JAVA/tester/ShowActivitiesBrowseServiceJSPTester.java
@@ -7,9 +7,7 @@ public class ShowActivitiesBrowseServiceJSPTester {
 
     public static void main(String[] args) {
         //Database initialization
-        MySqlDbConnection db = new MySqlDbConnection();
-        _0_SetDatabaseTest set = new _0_SetDatabaseTest();
-        set.setDatabase(db);
+        MySqlDbConnection db = MySqlDbConnection.getInstance();
         //Test JSON
         ShowActivitiesBrowseServiceJSP service = new ShowActivitiesBrowseServiceJSP();
         System.out.println(service.getShowActivitiesBrowseToJSONJSP(db, 1));
